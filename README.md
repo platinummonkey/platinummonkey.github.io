5ad4884aac79fb290ba4f02ec00b6d0936707c7b
Cody Lee <cody.lee@bendu.io>
2018-09-29 14:46:13 -0500

    reset the world since github is being a pain

---
+++ README.md
@@ -0,0 +1,6 @@
+# [accelerate-experience.com](http://accelerate-experience.com/)
+
+My personal blog where I talk about all things Startups, Entreprenuership, Software Architecture, Big Data and IOT.
+
+use hugo commands
+
---
+++ static/config.rb
@@ -0,0 +1,16 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path       = "/"
+css_dir         = "assets/css/"
+images_dir      = "assets/images/"
+javascripts_dir = "assets/js/"
+sass_dir        = "_assets/sass/"
+extra_dir       = "assets/posts/"
+
+output_style    = :nested #:expanded or :nested or :compact or :compressed
+color_output    = true
+relative_assets = false
+line_comments   = false
+environment     = :development
